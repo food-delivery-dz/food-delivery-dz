2023282e063b6626193e59d071df8295cff50cdd
food-delivery-dz <nourodev@gmail.com>
2024-08-13 22:13:13 +0100

    Add files via upload

---
+++ firebase-api.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB4lKXYxOogxq1o1sakdRA9ugTo8oKmTz0",
+  authDomain: "rb-delivery-268b1.firebaseapp.com",
+  databaseURL: "https://rb-delivery-268b1-default-rtdb.firebaseio.com",
+  projectId: "rb-delivery-268b1",
+  storageBucket: "rb-delivery-268b1.appspot.com",
+  messagingSenderId: "572920337404",
+  appId: "1:572920337404:web:7f392bb22bb514727e5879",
+};
---
+++ script.js
@@ -0,0 +1,73 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js"; 
+import { 
+  getDatabase, 
+  push, 
+  ref, 
+  update, 
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js"; 
+import { firebaseConfig } from "./firebase-api.js"; 
+ 
+// Initialize Firebase 
+const app = initializeApp(firebaseConfig); 
+const database = getDatabase(app); 
+ 
+// Selectors 
+const submitButton = document.querySelector(".submit-btn"); 
+const destinationInput = document.getElementById("destination"); 
+const orderDetailsInput = document.getElementById("order-details"); 
+const phoneNumberInput = document.getElementById("phone-number"); 
+const selectInput = document.querySelector(".minimal"); 
+ 
+// Functions 
+function getOrderData() { 
+  return { 
+    to: selectInput.value, 
+    order: orderDetailsInput.value, 
+    phone: phoneNumberInput.value, 
+    prix: 300, 
+    date: "", 
+    delivery: "", 
+    email: "nourodev@gmail.com", 
+    key: "", 
+    month: "", 
+    promot: "", 
+    static: "raning", 
+    time: "25 - 35 ", 
+    to_info: destinationInput.value, 
+    uid: "", 
+  }; 
+} 
+ 
+function sendOrder(order) { 
+  push(ref(database, "order"), order) 
+    .then((order) => { 
+      // update the same record to set uid 
+      update(ref(database, `order/${order.key}`),{uid: order.key}).then(()=>{ 
+        alert("تم إرسال الطلب بنجاح!"); 
+      }) 
+    }) 
+    .catch((error) => { 
+      console.error("Error writing new order to Firebase Database", error); 
+      alert("تم إرسال الطلب error!"); 
+    }); 
+ 
+  clearInputs(); 
+} 
+ 
+function clearInputs() { 
+  destinationInput.value = ""; 
+  orderDetailsInput.value = ""; 
+  phoneNumberInput.value = ""; 
+  selectInput.value = "-- اختر --"; 
+  destinationInput.focus(); 
+} 
+ 
+ 
+ 
+ 
+// Events 
+submitButton.onclick = function (e) { 
+  e.preventDefault(); 
+  const orderData = getOrderData(); 
+  sendOrder(orderData); 
+};
